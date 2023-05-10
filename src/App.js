1a26814ec4ad784fb774cb275e00867626f977e6
Cisco <ciscophrenzy@gmail.com>
2023-05-10 07:04:34 -0600

    Add required changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
